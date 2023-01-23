b57c8e53da7fb9c01c54de797c33fc60d8c33470
Semih Dağhan <daghansoftware@hotmail.com>
2023-01-23 14:50:22 +0300

    Client Credentials Grant-1 AuthServer'a IdentityServer4 paketi dahil edildi ApiResource: AuthServer'ın hangi Apilerden sorumlu olduğunu haber vermek ApiScope:  Üretilecek token değerinin API üzerindeki yetki alanını ifade eder. Client, Auth Service üzerinden elde ettiği token’da hangi scope değerlerine sahipse ancak o scope değerlerine sahip olan API’lara istekte bulunabilir.

---
+++ Apps/IdentityServer/IdentityServer.AuthServer/Config.cs
@@ -0,0 +1,30 @@
+﻿using IdentityServer4.Models;
+
+namespace IdentityServer.AuthServer
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("resource_api1"){Scopes={"api1.read","api1.write","api1.update"}},
+                new ApiResource("resource_api2"){Scopes={"api2.read","api2.write","api2.update"}}
+            };
+        }
+
+        public static IEnumerable<ApiScope> GetApiScopes() 
+        {
+            return new List<ApiScope>
+            {
+                new ApiScope("api1.read","Api 1 için okuma izni"),
+                new ApiScope("api1.write","Api 1 için yazma izni"),
+                new ApiScope("api1.update","Api 1 için update izni"),
+
+                new ApiScope("api2.read","Api 2 için okuma izni"),
+                new ApiScope("api2.write","Api 2 için yazma izni"),
+                new ApiScope("api2.update","Api 2 için update izni")
+            };
+        }
+    }
+}
