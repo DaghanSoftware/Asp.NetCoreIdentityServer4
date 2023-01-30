5043ba648d0d76130f8fdd506b1f758fafff9b87
Semih Dağhan <daghansoftware@hotmail.com>
2023-01-30 14:24:01 +0300

    Refresh Token Elde Etme

--- Apps/IdentityServer/IdentityServer.Client1/Program.cs
+++ Apps/IdentityServer/IdentityServer.Client1/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddAuthentication(options =>
     opts.GetClaimsFromUserInfoEndpoint = true;
     opts.SaveTokens = true;
     opts.Scope.Add("api1.read");
+    opts.Scope.Add("offline_access");
 });
 builder.Services.AddControllersWithViews();
 
