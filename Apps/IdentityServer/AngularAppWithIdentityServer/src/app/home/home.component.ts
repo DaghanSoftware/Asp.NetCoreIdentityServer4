355d04194cfbfe4963345ff5c4659b2fe6627499
Semih Dağhan <daghansoftware@hotmail.com>
2023-01-31 20:36:57 +0300

    Home component oluşturuldu.

---
+++ Apps/IdentityServer/AngularAppWithIdentityServer/src/app/home/home.component.ts
@@ -0,0 +1,31 @@
+import { Component,OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+ constructor(private authService:AuthService){}
+status: string | undefined;
+ ngOnInit() {
+  this.authService.userManager.getUser().then((user)=>{
+    if(user){
+      this.status="Hoşgeldiniz";
+      console.log(user);
+    }
+    else{
+      this.status="Giriş yapılmadı";
+    }
+   });
+}
+login():void{
+  this.authService.userManager.signinRedirect();
+}
+logout():void{
+  this.authService.userManager.signoutRedirect();
+}
+ 
+}
