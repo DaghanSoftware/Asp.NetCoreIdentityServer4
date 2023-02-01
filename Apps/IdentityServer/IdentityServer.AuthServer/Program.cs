2acfd61f6c36633e6a41051d3a42dde466bc4660
Semih Dağhan <daghansoftware@hotmail.com>
2023-02-01 16:09:41 +0300

    IdentityServer4.EntityFramework İmplementasyonu
    Neden konfigürasyon değerlerini fiziksel veritabanında tutmalıyız?
    In-memory’de tutulan değerler, olası restart durumlarında sıfırlanırlar. Dolayısıyla run time’da gerçekleştirilen tüm konfigürasyonlar bu şekilde kalıcılık özelliğini yitireceklerinden dolayı fiziksel veritabanlarına kaydedilerek garanti altına alınmalıdırlar.
    Ayrıca depolama işlemi in-memory’de yapıldığı sürece sisteme yeni bir client eklendiği zaman sistemin yeniden başlatılması gerekir. Böyle bir durumda client tanımlamalarının fiziksel veritabanından beslenmesi uygulama açısından da daha tercih edilir olacaktır.
    
    Hazır Context’ler
    IdentityServer4 veritabanı işlemlerini gerçekleştirebilmek için ConfigurationDbContext ve PersistedGrantDbContext olmak üzere iki farklı context nesnesi sunmaktadır. Bu context’lerin ne olduğunu sırasıyla açıklarsak eğer;
    
    ConfigurationDbContext
    Bu context ile; client, scope, user vs. konfigürasyonların hepsini veritabanında tutabiliriz.
    PersistedGrantDbContext
    Bu context ile ise ConfigurationDbContext’in işlevselliğiyle birlikte token ve authorize code vs. gibi değerleri veritabanında depolayabilmekteyiz.
    PersistedGrantDbContext sayesinde uygulama restart edildiğinde üretilen refresh token değeri kaybolmayacak yahut her iki context sayesinde de restart edilmeden sisteme yeni bir client, user vs. ekleyebileceğiz.
    
    IdentityServer4.EntityFramework kütüphanesini projeye dahil ettik

--- Apps/IdentityServer/IdentityServer.AuthServer/Program.cs
+++ Apps/IdentityServer/IdentityServer.AuthServer/Program.cs
@@ -3,6 +3,7 @@ using IdentityServer.AuthServer.Models;
 using IdentityServer.AuthServer.Repository;
 using IdentityServer.AuthServer.Services;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<ICustomUserRepository, CustomUserRepository>();
@@ -11,7 +12,20 @@ builder.Services.AddDbContext<CustomDbContext>(x =>
 {
     x.UseSqlServer(builder.Configuration.GetConnectionString("LocalDb"));
 });
+var assemblyName = typeof(Program).GetTypeInfo().Assembly.GetName().Name;
 builder.Services.AddIdentityServer()
+    //AddConfigurationStore fonksiyonu ile ‘ConfigurationDbContext’in konfigürasyonu
+    .AddConfigurationStore(opts => 
+    {
+        opts.ConfigureDbContext = c => c.UseSqlServer(builder.Configuration.GetConnectionString("LocalDb"),sqlOpts=>
+        sqlOpts.MigrationsAssembly(assemblyName));
+    })
+    //‘AddOperationalStore’ fonksiyonu ile ise ‘PersistedGrantDbContext’ konfigürasyonu
+    .AddOperationalStore(opts =>
+    {
+        opts.ConfigureDbContext = c => c.UseSqlServer(builder.Configuration.GetConnectionString("LocalDb"), sqlOpts =>
+        sqlOpts.MigrationsAssembly(assemblyName));
+    })
     .AddInMemoryApiResources(Config.GetApiResources())
     .AddInMemoryApiScopes(Config.GetApiScopes())
     .AddInMemoryClients(Config.GetClients())
