a65e1d8bf7d02ec9380f3f4a975c9d9178e759bc
Semih Dağhan <daghansoftware@hotmail.com>
2023-02-01 12:05:01 +0300

    AuthServer'a Resource Owner Credentials Grant tipinde bir client oluşturduk.

--- Apps/IdentityServer/IdentityServer.AuthServer/Config.cs
+++ Apps/IdentityServer/IdentityServer.AuthServer/Config.cs
@@ -144,7 +144,22 @@ namespace IdentityServer.AuthServer
                     AllowedCorsOrigins={"http://localhost:4200"},
                     PostLogoutRedirectUris={ "http://localhost:4200" }
 
-                }
+                },
+                 new Client()
+                {
+                    ClientId="Client1-ResourceOwner-Mvc",
+                    ClientName="Client1-ResourceOwner-Mvc MVC app uygulaması",
+                    ClientSecrets=new[] {new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    AllowedScopes={IdentityServerConstants.StandardScopes.OpenId,IdentityServerConstants.StandardScopes.Profile,IdentityServerConstants.StandardScopes.Email,
+                        "api1.read",IdentityServerConstants.StandardScopes.OfflineAccess,"CountryAndCity","Roles"},
+                    AccessTokenLifetime=2*60*60,
+
+                    AllowOfflineAccess=true,
+                    RefreshTokenUsage=TokenUsage.ReUse,
+                    RefreshTokenExpiration=TokenExpiration.Absolute,
+                    AbsoluteRefreshTokenLifetime=(int)(DateTime.Now.AddDays(60)-DateTime.Now).TotalSeconds,
+                },
             };
         }
     }
