b0a9d1edfe8dd21f614273f1fad56928a00ca9ea
Semih Dağhan <daghanex@hotmail.com>
2023-02-12 19:21:21 +0300

    SignUp Page Create

--- Apps/IdentityServer/Client1-ResourceOwner/Controllers/LoginController.cs
+++ Apps/IdentityServer/Client1-ResourceOwner/Controllers/LoginController.cs
@@ -36,7 +36,7 @@ namespace Client1_ResourceOwner.Controllers
             var password = new PasswordTokenRequest();
             password.Address = disco.TokenEndpoint;
             password.UserName = model.Email;
-            password.Password= model.Password;
+            password.Password = model.Password;
             password.ClientId = _configuration["Client1ResourceOwnerMvc:ClientId"];
             password.ClientSecret = _configuration["Client1ResourceOwnerMvc:ClientSecret"];
 
@@ -45,7 +45,7 @@ namespace Client1_ResourceOwner.Controllers
             var token = await client.RequestPasswordTokenAsync(password);
             if (token.IsError)
             {
-                ModelState.AddModelError("","Email veya şifreniz yanlış");
+                ModelState.AddModelError("", "Email veya şifreniz yanlış");
                 return View();
             }
 
@@ -65,9 +65,9 @@ namespace Client1_ResourceOwner.Controllers
             //3. parametre User.Identity.Name koduna karşılık kullanıcıdan gelen hangi claim ile eşleştirilmesi gerektiğini bildirmekte ve
             //4. parametre ise benzer şekilde kullanıcının authorize/rol yetkisinin yine kullanıcıdan gelen claim’lerden hangisiyle eşleşmesi gerektiğini belirlemektedir.
             //Burada ilgili claim değerlerinin elde edilebilmesi için Auth Server’da aşağıdaki Identity Resource’lerin eklenmesi gerektiğini unutmayınız…
-            ClaimsIdentity claimsIdentity = new ClaimsIdentity(userinfo.Claims, CookieAuthenticationDefaults.AuthenticationScheme,"name","role");
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity(userinfo.Claims, CookieAuthenticationDefaults.AuthenticationScheme, "name", "role");
             ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
-            
+
             //Burada giriş yapan kullanıcının elde ettiği authentication properties bilgilerini client uygulaması üzerinde güncellemekteyiz.
             //Hatta hatırlarsanız IdentityServer4 mimarisi için refresh token konusunu ele alırken de birebir aynı işlemi gerçekleştirmiştik.
             //Lakin ilgili makalede authentication property’leri
@@ -82,8 +82,20 @@ namespace Client1_ResourceOwner.Controllers
             });
 
             //Giriş işlemini yukarıda üretilen değerler eşliğinde gerçekleştiriyoruz.
-            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal,authenticationProperties);
-            return RedirectToAction("Index","User");
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal, authenticationProperties);
+            return RedirectToAction("Index", "User");
+        }
+
+        public IActionResult SignUp()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(UserSaveViewModel userSaveViewModel)
+        {
+            return RedirectToAction("Index");
         }
     }
 }
